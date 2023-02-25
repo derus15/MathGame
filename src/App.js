4c42e9740596e0d744b8c34807ce17cb4e99e789
derus15 <9608212661i@gmail.com>
2023-02-25 11:01:21 +0400

    add working timer v2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import Example from "./component/Example";
 import React, {useState} from "react";
-import Result from "./component/Result";
-import Interface from "./UI/Interface/Interface";
+import MyModal from "./UI/Modal/MyModal";
+import MyInput from "./UI/Input/MyInput";
+import MyButton from "./UI/Button/MyButton";
+
 
 function App() {
 
@@ -14,6 +16,7 @@ function App() {
 // Таймер сверху экрана
 // Увеличении числа при прокрутке колесиком, поскольку это input type number
 
+    const [end, setEnd] = useState(false);
     const [modal, setModal] = useState(false);
     const [counter, setCounter] = useState(0);
 
@@ -21,6 +24,10 @@ function App() {
         num_1: Math.floor(Math.random() * 100),
         num_2: Math.floor(Math.random() * 100),
     })
+
+    function resetCounter(){
+        setCounter(0);
+    }
 
     function refresh(){
         const refreshNum = {
@@ -30,38 +37,33 @@ function App() {
         setNumbers(refreshNum);
     }
 
-
-    function resetCounter(){
-        setCounter(0);
-    }
-
     function counterExample(){
         setCounter(counter+1);
     }
 
     function endSession(){
         console.log('конец');
+        setEnd(true);
         setModal(true);
-    }
-
-    function closeModal(){
-        setModal(false);
     }
 
   return (
       <div className={'container'}>
-          <h1 className={'header'}>MGame</h1>
-          <Interface/>
+          <h1 className={'header'} >MathGame</h1>
           <div className={'App'}>
               {modal
                   ?
-                    <Result counter={counter} closeModal={closeModal}/>
+              <div className={'result'}>
+                Примеров решено: {counter}
+                  <MyButton onClick={() => setModal(false)}>Хорошо</MyButton>
+              </div>
                   :<>
-                    <Example number={number} endSession={endSession} refresh={refresh}
-                     resetCounter={resetCounter} counter={counterExample}/>
-                    <div className={'answer'}>Ответ: {number.num_1 + number.num_2}</div>
+                      <Example setEnd={setEnd} number={number} endSession={endSession} refresh={refresh}
+                       resetCounter={resetCounter} counter={counterExample}/>
+                      <div className={'answer'}>Ответ: {number.num_1 + number.num_2}</div>
                   </>
               }
+
           </div>
       </div>
   );
