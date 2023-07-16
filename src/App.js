48e9af61cf101b9133bfec37355559172b45c3ec
Ivan Prikazchikov <89651706+derus15@users.noreply.github.com>
2023-07-16 14:30:21 +0400

    Исправление ошибки
    Исправление ошибки с импортом Auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import './UI/Themes/Themes.css';
 import React from 'react';
-import Auth from './component/pages/Auth';
 import { Route, Routes } from 'react-router-dom';
 import Home from './component/pages/Home';
 
@@ -11,7 +10,6 @@ function App() {
     return (
         <Routes>
             <Route path='/' element={<Home/>} />
-            {/*<Route path='/auth' element={<Auth/>} />*/}
         </Routes>
     );
 }
