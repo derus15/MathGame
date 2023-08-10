e1aec7887d3b1f2f4c86dda1f4be812770197c88
derus15 <9608212661i@gmail.com>
2023-08-10 16:59:15 +0400

    Добавление переменной окружения

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3020',
+    baseURL: process.env.AXIOS_CONNECT,
 });
 
 instance.interceptors.request.use((config) => {
