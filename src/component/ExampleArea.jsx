77a31fd348f5e38ae3105c5a92477c91eafeb1cb
derus15 <9608212661i@gmail.com>
2023-08-10 17:19:13 +0400

    Добавление переменной окружения
    Попытка вторая

--- src/component/ExampleArea.jsx
+++ src/component/ExampleArea.jsx
@@ -4,6 +4,9 @@ import StandardTimer from './Timers/StandardTimer';
 import Example from './Example';
 import SprintTimer from './Timers/SprintTimer';
 import { useSelector } from 'react-redux';
+import useGenerateExample from '../hooks/useGenerateExample';
+import Tooltip from '../UI/Tooltip/Tooltip';
+import { resetSprintCounter, resetStandardCounter } from '../redux/Slices/dataSlice';
 
 const ExampleArea = ({ endSession, sessionProgress, setSessionProgress }) => {
 
@@ -11,6 +14,8 @@ const ExampleArea = ({ endSession, sessionProgress, setSessionProgress }) => {
     const [sign, SetSign] = useState(signList[Math.floor(Math.random() * signList.length)]);
     const [answer, setAnswer] = useState();
     const gameMode = useSelector(state => state.interface.mode);
+    // const [date, setDate] = useState();
+    // const [instruction, setInstruction] = useState(true);
 
     const [number, setNumbers] = useState({
         num_1: Math.floor(Math.random() * 100),
@@ -38,6 +43,8 @@ const ExampleArea = ({ endSession, sessionProgress, setSessionProgress }) => {
     function startSession() {
         if (!sessionProgress) {
             setSessionProgress(true);
+            // setDate(new Date().getTime());
+            // setInstruction(false);
         }
     }
 
@@ -46,6 +53,9 @@ const ExampleArea = ({ endSession, sessionProgress, setSessionProgress }) => {
             changeSign();
             refresh();
             e.target.value = '';
+            // let dateNow = new Date().getTime();
+            // let timeSpend = (dateNow - date) / 1000;
+            // setDate(new Date().getTime());
         }
     }
 
@@ -63,6 +73,21 @@ const ExampleArea = ({ endSession, sessionProgress, setSessionProgress }) => {
                               signal={answer}
                               sessionProgress={sessionProgress} />
             </div>
+            {/*{(instruction)*/}
+            {/*    ?*/}
+            {/*    <div style={{*/}
+            {/*        color: 'var(--base-color)',*/}
+            {/*        position: 'absolute',*/}
+            {/*        left: '33%',*/}
+            {/*        top: '170px',*/}
+            {/*        fontSize: '19px',*/}
+            {/*    }}*/}
+            {/*         className={'text-animation'}>*/}
+            {/*        Для начала сессии нажмите на поле ввода<br/>или используйте Tab*/}
+            {/*    </div>*/}
+            {/*    :*/}
+            {/*    <></>*/}
+            {/*}*/}
         </div>
     );
 };
--- src/hooks/useGenerateExample.jsx
+++ src/hooks/useGenerateExample.jsx
@@ -1,21 +1,8 @@
-import React, { useEffect, useMemo, useState } from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 
-const UseGenerateExample = () => {
+const UseGenerateExample = ({ number, sign, setAnswer }) => {
 
-    const signList = useSelector(state => state.interface.signList);
-    const [sign, SetSign] = useState(signList[Math.floor(Math.random() * signList.length)]);
-    const [answer, setAnswer] = useState();
-    const [answer, setAnswer] = useState();
     const [example, setExample] = useState();
-
-    const [number, setNumbers] = useState({
-        num_1: Math.floor(Math.random() * 100),
-        num_2: Math.floor(Math.random() * 100),
-    });
-
-    let num_1 = number.num_1;
-    let num_2 = number.num_2;
 
     const signFunction = {
         '+': (a, b) => a + b,
@@ -24,54 +11,44 @@ const UseGenerateExample = () => {
         '/': (a, b) => a / b,
     };
 
-    useMemo(() => {
-        refresh();
-        changeSign();
-    }, [signList]); // gameMode
+    function generateExample() {
 
-    function changeSign() {
-        let signNumber = signList[Math.floor(Math.random() * signList.length)];
-        SetSign(signNumber);
-    }
+        let num_1 = number.num_1;
+        let num_2 = number.num_2;
 
-    function refresh() {
-        const refreshNum = {
-            num_1: Math.floor(Math.random() * 100),
-            num_2: Math.floor(Math.random() * 100),
-        };
-        setNumbers(refreshNum);
-    }
+        if ((sign === '-') && (num_1 < num_2)) {
+            [num_1, num_2] = [num_2, num_1];
 
-    function answered(e) {
-        if (e.target.value === answer) {
-            changeSign();
-            refresh();
-            e.target.value = '';
-        }
-    }
+        } else if (sign === '/') {
 
-    if ((sign === '-') && (num_1 < num_2)) {
-        [num_1, num_2] = [num_2, num_1];
-    } else if (sign === '/') {
+            if (num_1 === 0) {
+                num_1 += 1;
+            }
 
-        if (num_1 === 0) {
-            num_1 += 1;
+            if (num_1 > 10 && num_1 < 100) {
+                num_2 = Math.floor(num_2 / 10) + 1;
+            }
+
+            let answer = num_1 * num_2;
+            [num_1, answer] = [answer, num_1];
+
         }
 
-        if (num_1 > 10 && num_1 < 100) {
-            num_2 = Math.floor(num_2 / 10) + 1;
-        }
+        let answerExample = String(signFunction[sign](num_1, num_2));
+        setAnswer(answerExample);
+        setExample(`${num_1} ${sign} ${num_2} =`);
 
-        let answer = num_1 * num_2;
-        [num_1, answer] = [answer, num_1];
+        return [example];
 
     }
 
-    setAnswer(String(signFunction[sign](num_1, num_2)));
-    setExample(`${num_1} ${sign} ${num_2} =`);
+    generateExample()
 
-    return [example, answer];
+    useEffect(() => {
+        generateExample()
+    },[sign]);
 
-};
+}
 
-export default UseGenerateExample;
+
+    export default UseGenerateExample;
