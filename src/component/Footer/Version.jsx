fdd5a222a9004b538696eb0a39c0e0b41a55dd6c
derus15 <9608212661i@gmail.com>
2023-09-11 14:35:01 +0400

    Добавлена кнопка "Запомни меня" в окне входа

--- src/component/Footer/Version.jsx
+++ src/component/Footer/Version.jsx
@@ -11,7 +11,7 @@ const Version = () => {
 
     return (
         <div>
-            <div className={'extra'} onClick={showModalVersion}>v 2.0.1</div>
+            <div className={'extra'} onClick={showModalVersion}>v 2.0.4</div>
             {(modalVersion)
                 ?
                 <MyModal
--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Auth.module.css';
 import 'react-toastify/dist/ReactToastify.css';
 import '../../UI/Toaster/Toaster.css';
@@ -9,21 +9,37 @@ import { useForm } from 'react-hook-form';
 import { authData, fetchAuth } from '../../redux/Slices/authSlice';
 import { Navigate } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
+import Checkbox from '../../UI/Checkbox/Checkbox';
 
 const Login = () => {
 
+    const [isRemember, setIsRemember] = useState(JSON.parse(localStorage.getItem('remember')) || false);
     const dispatch = useDispatch();
     const isAuth = useSelector(authData);
 
     const status = useSelector(state => state.auth.statusLog);
     const isLoading = status === 'loading';
 
-    const { handleSubmit, register } = useForm({ mode: 'onChange'});
+    const { handleSubmit, register } = useForm({
+        mode: 'onChange',
+
+        defaultValues: {
+            email: isRemember ? localStorage.getItem('email') : '',
+            password: isRemember ? localStorage.getItem('password') : '',
+        },
+    });
 
     const onSubmit = async (values) => {
 
         const data = await dispatch(fetchAuth(values));
         const error = data.payload;
+
+        localStorage.setItem('remember', isRemember);
+
+        if (isRemember) {
+            localStorage.setItem('email', values.email);
+            localStorage.setItem('password', values.password);
+        }
 
         if (error && error.response) {
 
@@ -45,8 +61,8 @@ const Login = () => {
             localStorage.setItem('token', data.payload.token);
         }
 
-        if(!('token' in data.payload) && !(error && error.response)){
-            toast.error('Сервер не отвечает. Попробуйте позже')
+        if (!('token' in data.payload) && !(error && error.response)) {
+            toast.error('Сервер не отвечает. Попробуйте позже');
         }
     };
 
@@ -60,7 +76,8 @@ const Login = () => {
                 <div className={style.loginHeader}>Логин</div>
                 <LoginInput placeholder={'Почта'} {...register('email')} type={'email'}></LoginInput>
                 <LoginInput placeholder={'Пароль'} {...register('password')}></LoginInput>
-                <LoginButton>Войти</LoginButton>
+                <Checkbox label={'Запомни меня'} checked={isRemember} setIsChecked={setIsRemember} />
+                <LoginButton style={{margin:'0'}}>Войти</LoginButton>
                 <div className={isLoading ? [style.loader, style.positionLoginLoader].join(' ') : ''}></div>
                 <ToastContainer
                     position='top-center'
