cfd055714b4901bae3b7c31a031172ce80044b9b
derus15 <9608212661i@gmail.com>
2023-12-15 22:04:16 +0400

    build: ProgressPlugin перенесен в dev режим, так как замедляет сборку

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,10 +5,9 @@ import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from '../../src/types/config';
 
-export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
+export function buildPlugins({ paths, isDev }: BuildOptions): webpack.Configuration['plugins'] {
 
-    const plugins = [
-        new webpack.ProgressPlugin(), // Может замедлять сборку в проде - убрать в дев режим
+    const plugins: webpack.Configuration['plugins'] = [
         // ниже без указания template, html создается заново
         new HTMLWebpackPlugin({
             template: paths.html,
@@ -26,6 +25,7 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
 
     if (isDev) {
         plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
+        plugins.push(new webpack.ProgressPlugin());
     }
 
     return plugins;
