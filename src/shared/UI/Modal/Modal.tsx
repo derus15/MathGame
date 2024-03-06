6ad34c1e4acd93ec92419c0ea8ba200433be1d5b
derus15 <9608212661i@gmail.com>
2024-03-06 22:00:53 +0400

    feat: Добавление Escape как горячей клавиши для закрытия модального окна + правка некоторых стилей приложения

--- src/shared/UI/Modal/Modal.tsx
+++ src/shared/UI/Modal/Modal.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, FC, memo, ReactNode, SetStateAction } from 'react';
+import React, { Dispatch, FC, memo, ReactNode, SetStateAction, useEffect } from 'react';
 import style from './Modal.module.css';
 import { classNames } from 'shared/lib/classNames/classNames';
 
@@ -9,21 +9,38 @@ interface modalProps {
     className?: string;
 }
 
-const Modal:FC<modalProps> = ({ children, visible, setVisible, className }) => (
+const Modal:FC<modalProps> = ({ children, visible, setVisible, className }) => {
 
-    visible && (
-        <div
-            className={classNames(style.myModal, { [style.myModalActive]: visible })}
-            onClick={() => setVisible(false)}
-        >
+    const handleCloseModal = (e: KeyboardEvent) => {
+        if (visible && e.key === 'Escape') {
+            setVisible(false);
+        }
+    };
+    
+    useEffect(() => {
+        window.addEventListener('keydown', handleCloseModal);
+        console.log('monte');
+        return () => {
+            window.removeEventListener('keydown', handleCloseModal);
+            console.log('разъебало');
+        };
+    }, [visible]);
+    
+    return (   
+        visible && (
             <div
-                className={classNames(style.myModalContent, {}, [className])}
-                onClick={(e) => e.stopPropagation()}
+                className={classNames(style.myModal, { [style.myModalActive]: visible })}
+                onClick={() => setVisible(false)}
             >
-                {children}
+                <div
+                    className={classNames(style.myModalContent, {}, [className])}
+                    onClick={(e) => e.stopPropagation()}
+                >
+                    {children}
+                </div>
             </div>
-        </div>
-    )
-);
+        ));
+
+};
 
 export default memo(Modal);
