4b336c0897feaf40e5780299b2ad7b422dec87cd
derus15 <9608212661i@gmail.com>
2024-04-02 22:18:32 +0400

    fix: Включение функции сохранения последней сессии после регистрации пользователя

--- src/entities/SessionData/model/api/saveSessionDataInLocalStorage.ts
+++ src/entities/SessionData/model/api/saveSessionDataInLocalStorage.ts
@@ -1,4 +1,5 @@
 import { store } from 'app/Providers/Store/store';
+import { SessionDataSaveSchema } from 'entities/SessionData/model/services/sendSessionData';
 
 export const saveSessionDataInLocalStorage = () => {
 
@@ -7,15 +8,21 @@ export const saveSessionDataInLocalStorage = () => {
     const { isAuth } = state.user;
     const { sessionPoints } = state.sessionData;
     const { sessionTime } = state.sessionData;
+    const { sessionEPS } = state.sessionData;
+    const { modificationsList } = state.modifications;
 
+    const sessionUnexpectedEnd = state.session.unexpectedEnd;
     const sessionGameMode = state.interface.gameMode;
     const sessionSignList = state.interface.signList;
 
-    const actualSessionData = {
+    const actualSessionData: SessionDataSaveSchema = {
         mode: sessionGameMode,
         number: sessionPoints,
         time: sessionTime,
         sign: sessionSignList,
+        eps: sessionEPS,
+        modifications: modificationsList,
+        unexpectedEnd: sessionUnexpectedEnd,
     };
 
     if (!isAuth) {
