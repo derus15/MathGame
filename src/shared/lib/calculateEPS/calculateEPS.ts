695a527eccd0fd1ab2525862a8bdfb754867502c
derus15 <9608212661i@gmail.com>
2024-04-07 22:42:11 +0400

    fix: Исправление неверного подсчета EPS при сессии длительностью менее 1 секунды

--- src/shared/lib/calculateEPS/calculateEPS.ts
+++ src/shared/lib/calculateEPS/calculateEPS.ts
@@ -1,5 +1,13 @@
 export const calculateEPS = (example: number, time: number) => {
 
+    if (time === 0 && example === 0) {
+        return '0.00';
+    }
+
+    if (time === 0 && example === 1) {
+        return '1.00';
+    }
+    
     if (time === 0) {
         return '1.00';
     }
