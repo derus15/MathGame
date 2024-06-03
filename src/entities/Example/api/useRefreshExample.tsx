d5ae3b2e2225d1f31e61fc3a961bc66e117e0971
derus15 <9608212661i@gmail.com>
2024-06-03 14:44:35 +0400

    feat: Упрощение системы генерации сидов. Функция опирается на количество итераций, приписывая цифру в конце

--- src/entities/Example/api/useRefreshExample.tsx
+++ src/entities/Example/api/useRefreshExample.tsx
@@ -1,32 +1,33 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { useState } from 'react';
 import { getInterfaceSignsList } from 'widgets/Interface';
-import { generateSeed } from 'shared/lib/generateExampleSeed/generateExampleSeed';
 import generateExample from 'shared/lib/generateExample/generateExample';
 import { exampleActions } from '../model/slice/exampleSlice';
+import { generateSeed } from 'shared/lib/generateExampleSeed/generateExampleSeed';
 
 export const useRefreshExample = () => {
 
     const dispatch = useDispatch();
     const signList = useSelector(getInterfaceSignsList);
 
-    const [currentSeed, setCurrentSeed] = useState(generateSeed(signList));
+    const [initialSeed, setInitialSeed] = useState(generateSeed(signList));
     const [iteration, setIteration] = useState(0);
 
     const refreshExample = () => {
 
         const { example, answer, sign, seed, nextIteration } = generateExample({
             signList,
-            seed: currentSeed,
+            seed: initialSeed,
             iteration,
         });
 
-        setCurrentSeed(seed);
         setIteration(nextIteration);
         dispatch(exampleActions.setAnswer(answer));
         dispatch(exampleActions.setExample(example));
         dispatch(exampleActions.setSign(sign));
     };
 
-    return { refreshExample };
+    const retry = () => {};
+
+    return { refreshExample, retry };
 };
