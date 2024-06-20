5d8e9ba2048a03f7ffc4b58d1feddbe03cda87f7
derus15 <9608212661i@gmail.com>
2024-06-20 13:35:32 +0400

    fix: Исправление ошибки с пустым полем для мат. знака в PersonalSeed + подсветка свг элемента при активном input

--- src/shared/lib/decodeBase64/isDecodeBase64.ts
+++ src/shared/lib/decodeBase64/isDecodeBase64.ts
@@ -1,5 +1,9 @@
 export const isDecodeBase64 = (input: string): string => {
     try {
+        const error = new Error('no text in input');
+        if (input === '') {
+            throw error;
+        }
         return atob(input);
     } catch (e) {
         return '-1';
