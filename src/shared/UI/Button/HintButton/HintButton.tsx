b873660e5a74dc793d592d3215a15451d3e7390c
derus15 <9608212661i@gmail.com>
2024-07-06 22:26:22 +0400

    feat: Доработка элемента HintButton, добавление пропса для подсказки при наведении

--- src/shared/UI/Button/HintButton/HintButton.tsx
+++ src/shared/UI/Button/HintButton/HintButton.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, useEffect, useRef } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import style from './HintButton.module.css';
 
@@ -8,11 +8,25 @@ interface HintButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string,
 }
 
-export const HintButton = ({ title = '>>', text, className }: HintButtonProps) => (
-    <button
-        type="button"
-        className={classNames(style.hintButton, {}, [className])}
-    >
-        {title}
-    </button>
-);
+export const HintButton = ({ title = '>>', text, className, ...props }: HintButtonProps) => {
+
+    const buttonRef = useRef<HTMLButtonElement>(null);
+
+    useEffect(() => {
+        if (buttonRef.current && text) {
+            buttonRef.current.style.setProperty('--hint-content', `"${text}"`);
+        }
+    }, [text]);
+
+    return (
+        <button
+            type="button"
+            className={classNames(style.hintButton, {}, [className])}
+            ref={buttonRef}
+            {...props}
+        >
+            {title}
+        </button>
+
+    );
+};
