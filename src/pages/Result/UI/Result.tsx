440c3a9e0f823f5c87877d6e9425f11bdfdee4dc
derus15 <9608212661i@gmail.com>
2024-07-17 14:25:29 +0400

    refactor: Перенос статистики сессии в компонент ResultStat

--- src/pages/Result/UI/Result.tsx
+++ src/pages/Result/UI/Result.tsx
@@ -4,35 +4,26 @@ import style from './Result.module.css';
 import { ExampleButton } from 'shared/UI/Button/ExampleButton/ExampleButton';
 import { getUnexpectedEndText, sessionActions } from 'entities/Session';
 import {
-    getSessionPoints, getSessionTime,
     sessionDataActions, useSendSessionData,
 } from 'entities/SessionData';
 import { getIsAuth } from 'entities/User';
 import { useAppDispatch } from 'shared/lib/hooks/reduxHooks/reduxHooks';
 import { ExampleModal } from './ExampleModal/ExampleModal';
 import { PageLayout } from 'shared/UI/PageLayout/PageLayout';
-import { ResultItem } from './ResultItem/ResultItem';
 import { InviteRegister } from './InviteRegister/InviteRegister';
-import { getInterfaceGameMode } from 'widgets/Interface';
-import { getCurrentRound, hungerModeActions } from 'features/GameMods/HungerMode';
-import { exampleActions, getInitialSeed, getIsPersonalSeed, getIsRetrySession } from 'entities/Example';
+import { hungerModeActions } from 'features/GameMods/HungerMode';
+import { exampleActions, getIsPersonalSeed, getIsRetrySession } from 'entities/Example';
 import { toast } from 'react-toastify';
-import { useCalculateEPS } from 'shared/lib/hooks/useCalculateEPS';
+import { ResultStat } from 'pages/Result/UI/ResultStat/ResultStat';
 
 export const Result = () => {
 
     const isAuth = useSelector(getIsAuth);
-    const gameMode = useSelector(getInterfaceGameMode);
-    const numberResult = useSelector(getSessionPoints);
-    const sessionTime = useSelector(getSessionTime);
-    const round = useSelector(getCurrentRound);
     const sessionTextEnd = useSelector(getUnexpectedEndText);
     const isRetry = useSelector(getIsRetrySession);
-    const seed = useSelector(getInitialSeed);
     const isPersonalSeed = useSelector(getIsPersonalSeed);
 
     const dispatch = useAppDispatch();
-    const eps = useCalculateEPS();
     const { sendSessionData } = useSendSessionData();
     const isCustomSession = isPersonalSeed || isRetry;
 
@@ -66,11 +57,7 @@ export const Result = () => {
             <div className={style.resultContainer}>
                 <h3 className={style.title}>{sessionTextEnd}</h3>
                 <div className={style.result}>
-                    <ResultItem title="Примеров решено:" value={numberResult} />
-                    {gameMode === 'Голод' && <ResultItem title="Раундов завершено:" value={round} />}
-                    <ResultItem title="Ваше время:" value={sessionTime} isTime />
-                    <ResultItem title="ПВС:" value={eps} description="Примеров в секунду" isEPS />
-                    <ResultItem title="Сид:" value={seed} />
+                    <ResultStat />
                     <div className={style.buttonContainer}>
                         <ExampleModal />
                         <ExampleButton onClick={retrySessionHandle}>Повторить</ExampleButton>
--- src/pages/Result/UI/ResultStat/ResultStat.tsx
+++ src/pages/Result/UI/ResultStat/ResultStat.tsx
@@ -15,14 +15,14 @@ export const ResultStat = () => {
     const round = useSelector(getCurrentRound);
     const seed = useSelector(getInitialSeed);
     const eps = useCalculateEPS();
-    console.log('resultStat');
+
     return (
-        <div>
+        <>
             <ResultItem title="Примеров решено:" value={numberResult} />
             {gameMode === 'Голод' && <ResultItem title="Раундов завершено:" value={round} />}
             <ResultItem title="Ваше время:" value={sessionTime} isTime />
             <ResultItem title="ПВС:" value={eps} description="Примеров в секунду" isEPS />
             <ResultItem title="Сид:" value={seed} />
-        </div>
+        </>
     );
 };
