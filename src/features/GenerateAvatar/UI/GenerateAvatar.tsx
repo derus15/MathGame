497291e6dbb4e3570773abeb9d9f1ed0a9d934e4
derus15 <9608212661i@gmail.com>
2024-09-02 13:06:30 +0400

    feat: уменьшение сетки для генерации Аватарки

--- src/features/GenerateAvatar/UI/GenerateAvatar.tsx
+++ src/features/GenerateAvatar/UI/GenerateAvatar.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import style from './GenerateAvatar.module.css';
 
 const generateRandomColor = (isMono: boolean = false) => {
-    const letters = isMono ? 'FFFFFFFFFFFFFF' : '0123456789ABCDEF';
+
+    const letters = isMono ? '000000000000000' : '0123456789ABCDEF';
     let color = '#';
     // eslint-disable-next-line no-plusplus
     for (let i = 0; i < 6; i++) {
@@ -12,15 +13,14 @@ const generateRandomColor = (isMono: boolean = false) => {
 };
 
 export const AvatarGenerator = () => {
-    const squares = Array.from({ length: 120 }, () => generateRandomColor(true));
+
+    const squares = Array.from({ length: 145 }, () => generateRandomColor(true));
 
     return (
-        <div className={style.avatarContainer}>
-            <div className={style.avatarCircle}>
-                {squares.map((color, index) => (
-                    <div key={index} className={style.square} style={{ backgroundColor: color }} />
-                ))}
-            </div>
+        <div className={style.avatarCircle}>
+            {squares.map((color, index) => (
+                <div key={index} className={style.square} style={{ backgroundColor: color }} />
+            ))}
         </div>
     );
 };
