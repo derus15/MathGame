875cb819c67a81b0dbe34c9d73ae249286ea683a
derus15 <9608212661i@gmail.com>
2024-09-16 11:20:01 +0400

    hot: hot fix

--- src/pages/Account/UI/Account.tsx
+++ src/pages/Account/UI/Account.tsx
@@ -1,37 +1,36 @@
 import React from 'react';
 import style from './Account.module.css';
 import { AccountUserInfo, useGetAccountInfoQuery } from 'widgets/AccountUserInfo';
-import Loading from 'shared/UI/Loading/Loading';
+import { Loading } from 'shared/UI/Loading/Loading';
 import {
     HighlightsBoards, useGetHighlightBoardQuery,
 } from 'widgets/AccountHighlightsBoards';
 import { Footer } from 'widgets/Footer';
-import { PageLayout } from 'shared/UI/PageLayout/PageLayout';
+import { useGetUserAvatarQuery } from 'features/GenerateAvatar';
 
 const Account = () => {
 
     const { isLoading: accountLoadingStatus } = useGetAccountInfoQuery();
     const { isLoading: boardLoadingStatus } = useGetHighlightBoardQuery();
-    const isLoading = accountLoadingStatus || boardLoadingStatus;
+    const { isLoading: userAvatarLoadingStatus } = useGetUserAvatarQuery();
+    const isLoading = accountLoadingStatus || boardLoadingStatus || userAvatarLoadingStatus;
 
     if (isLoading) {
         return <Loading />;
     }
 
     return (
-        <PageLayout hideFooter>
+        <>
             <div className={style.mainContainer}>
                 <AccountUserInfo />
                 <HighlightsBoards />
-                <div className={style.anotherTiles}>
-                    <div className={style.tiles} />
+                <div className={style.comingSoonContainer}>
+                    <div className={style.comingSoonTiles} />
+                    <span className={style.comingSoonTitle}>Скоро будет добавлено...</span>
                 </div>
-                <div className={style.soonContainer}>
-                    <span className={style.comingSoon}>Скоро будет добавлено...</span>
-                </div>
-                <Footer className={style.footer} />
             </div>
-        </PageLayout>
+            <Footer className={style.footer} />
+        </>
     );
 };
 
