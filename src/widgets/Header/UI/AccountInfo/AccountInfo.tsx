3bf8138b44032809e12957c8d72b0bb987f799c3
derus15 <9608212661i@gmail.com>
2024-09-18 17:01:21 +0400

    feat: Добавление Loader при инициализации пользователя

--- src/widgets/Header/UI/AccountInfo/AccountInfo.tsx
+++ src/widgets/Header/UI/AccountInfo/AccountInfo.tsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import style from './AccountInfo.module.css';
 import ProfileSvg from '/public/assets/profile.svg';
 import LogoutSvg from '/public/assets/logout.svg';
-import { getIsAuth, userActions } from 'entities/User';
+import { getIsAuth, useGetInitAuthDataQuery, userActions } from 'entities/User';
 import { useAppDispatch } from 'shared/lib/hooks/reduxHooks/reduxHooks';
 import { Loader } from 'shared/UI/Loader/Loader';
 import { useGetHeaderNameQuery } from '../../api/HeaderNameApi';
@@ -12,7 +12,9 @@ import { useGetHeaderNameQuery } from '../../api/HeaderNameApi';
 export const AccountInfo = () => {
 
     const isAuth = useSelector(getIsAuth);
-    const { data: user, isLoading } = useGetHeaderNameQuery(undefined, { skip: !isAuth });
+    const token = localStorage.getItem('token');
+    const { data: user, isLoading: isLoadingName } = useGetHeaderNameQuery(undefined, { skip: !isAuth });
+    const { isLoading: isLoadingInit } = useGetInitAuthDataQuery(undefined, { skip: !token });
     const dispatch = useAppDispatch();
 
     const Logout = () => {
@@ -25,17 +27,20 @@ export const AccountInfo = () => {
                 <Link className={style.linkAccount} to="/account">
                     <span className={style.username}>{user?.name}</span>
                 </Link>
-                {isLoading && <Loader isLoading={isLoading} className={style.loader} />}
+                {isLoadingName && <Loader isLoading={isLoadingName} className={style.loaderName} />}
                 <Link to="/account" onClick={Logout} className={style.logoutContainer}>
                     <LogoutSvg />
                 </Link>
             </div>
         ) : (
-            <Link className={style.linkProfile} to="/auth">
-                <svg className={style.profile}>
-                    <ProfileSvg />
-                </svg>
-            </Link>
+            <div className={style.containerAccountInfo}>
+                {isLoadingInit && <Loader isLoading={isLoadingInit} className={style.loaderInit} />}
+                <Link className={style.linkProfile} to="/auth">
+                    <svg className={style.profile}>
+                        <ProfileSvg />
+                    </svg>
+                </Link>
+            </div>
         )
     );
 };
