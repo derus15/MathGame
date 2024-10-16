ba986f38f286638c217e86537e44006f004d0e01
derus15 <9608212661i@gmail.com>
2024-10-16 20:25:46 +0400

    feat: Добавление функции копирования для адреса электронной почты в Footer

--- src/widgets/Footer/UI/elements/ErrorReport.tsx
+++ src/widgets/Footer/UI/elements/ErrorReport.tsx
@@ -2,13 +2,22 @@ import React, { useState } from 'react';
 import Modal from 'shared/UI/Modal/Modal';
 import style from '../Footer.module.css';
 import { OutlineButton } from 'shared/UI/Button/OutlineButton/OutlineButton';
+import { copyTextToClipboard } from 'shared/lib/copyTextToClipboard/copyTextToClipboard';
+import { toast } from 'react-toastify';
 
 const ErrorReport = () => {
+
     const [modalError, setModalError] = useState(false);
+    const email = 'mathgame.problem@gmail.com';
 
     function showModalError() {
         setModalError(true);
     }
+
+    const handleCopyToClipboard = () => {
+        copyTextToClipboard(email);
+        toast.error('Адрес скопирован');
+    };
 
     return (
         <>
@@ -20,7 +29,12 @@ const ErrorReport = () => {
             </OutlineButton>
             <Modal visible={modalError} setVisible={setModalError}>
                 Если вы нашли ошибку, то пишите на почту
-                <span className={style.errorEmail}> mathgame.problem@gmail.com</span>
+                <span
+                    className={style.errorEmail}
+                    onClick={handleCopyToClipboard}
+                >
+                    {email}
+                </span>
             </Modal>
         </>
     );
