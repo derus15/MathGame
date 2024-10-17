e931bb0d4d8c78da3d8933cc6f16306499d00c44
derus15 <9608212661i@gmail.com>
2024-10-17 21:08:07 +0400

    feat: Обновление rtkApi под jwt авторизацию с refresh токеном

--- src/shared/api/rtkApi.ts
+++ src/shared/api/rtkApi.ts
@@ -1,20 +1,37 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { BaseQueryFn, createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const url = __IS_DEV__ ? 'http://localhost:3020' : 'https://math-game.ru/';
 
+export const baseQuery = fetchBaseQuery({
+    baseUrl: url,
+    credentials: 'include',
+    prepareHeaders: (headers) => {
+        const accessToken = localStorage.getItem('token');
+        if (accessToken) {
+            headers.set('Authorization', accessToken);
+        }
+        return headers;
+    },
+});
+
+const baseQueryWithReauth: BaseQueryFn = async (args, api, extraOptions) => {
+    let result = await baseQuery(args, api, extraOptions);
+
+    if (result.error?.status === 403 && args.url !== '/auth/refresh') {
+
+        const { data }: any = await baseQuery('/auth/refresh', api, extraOptions);
+        localStorage.setItem('token', data.accessToken);
+
+        result = await baseQuery(args, api, extraOptions);
+
+    }
+
+    return result;
+};
+
 export const rtkApi = createApi({
-    reducerPath: 'rtkApi',
-    baseQuery: fetchBaseQuery(
-        { baseUrl: url,
-            prepareHeaders: (headers) => {
-                const token = localStorage.getItem('token');
-                if (token) {
-                    headers.set('Authorization', token);
-                }
-                return headers;
-            },
-        },
-    ),
     tagTypes: ['Session', 'Account', 'Avatar'],
-    endpoints: (builder) => ({}),
+    reducerPath: 'rtkApi',
+    baseQuery: baseQueryWithReauth,
+    endpoints: () => ({}),
 });
