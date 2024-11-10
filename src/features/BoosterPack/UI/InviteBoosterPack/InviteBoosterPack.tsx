365f5ad71d9102c12b36b06a4d0d8cd1b20fe0fe
derus15 <9608212661i@gmail.com>
2024-11-10 20:35:23 +0400

    feat: Написание формулы для вычисления шанса выпадения бустер пака, закрытие InviteBoosterPack после открытия пака

--- src/features/BoosterPack/UI/InviteBoosterPack/InviteBoosterPack.tsx
+++ src/features/BoosterPack/UI/InviteBoosterPack/InviteBoosterPack.tsx
@@ -3,25 +3,33 @@ import { useSelector } from 'react-redux';
 import { getIsAuth } from 'entities/User';
 import { BoosterPack } from '../BoosterPack/BoosterPack';
 import style from './InviteBoosterPack.module.css';
+import { useCalculateChanceBooster } from '../../hooks/useCalculateChanceBooster';
+import { OutlineButton } from 'shared/UI/Button/OutlineButton/OutlineButton';
 
 export const InviteBoosterPack = () => {
 
     const [isOpen, setIsOpen] = useState(false);
+    const [isTextVisible, setIsTextVisible] = useState(true);
+    const { calculateBoosterPackChance } = useCalculateChanceBooster();
+    const [isVisibleBoosterPack, setIsVisibleBoosterPack] = useState(calculateBoosterPackChance);
     const isAuth = useSelector(getIsAuth);
 
     const handleOpenBoosterPack = () => {
         setIsOpen(true);
+        setIsTextVisible(false);
     };
-    
+
     return (
-        isAuth && (
+        (isAuth && isVisibleBoosterPack) && (
             <div className={style.inviteContainer}>
-                <span
-                    className={style.inviteTitle}
-                    onClick={handleOpenBoosterPack}
-                >
-                    Вам доступно открытие пака
-                </span>
+                {isTextVisible && (
+                    <OutlineButton
+                        className={style.inviteTitle}
+                        onClick={handleOpenBoosterPack}
+                    >
+                        Вам доступно открытие пака
+                    </OutlineButton>
+                )}
                 <BoosterPack isOpenPack={isOpen} />
             </div>
         )
--- src/features/BoosterPack/hooks/useCalculateChanceBooster.tsx
+++ src/features/BoosterPack/hooks/useCalculateChanceBooster.tsx
@@ -1,3 +1,34 @@
+import { useSelector } from 'react-redux';
+import { getSessionPoints, getSessionTimeSeconds } from 'entities/SessionData';
+import { getModificationsList } from 'features/Modifications';
+import { getExampleSignList } from 'entities/Example';
+
 export const useCalculateChanceBooster = () => {
     
+    const baseChance = 10;
+    const baseMultiplier = 1.08;
+    const maxChance = 60;
+
+    const sessionTime = useSelector(getSessionTimeSeconds) / 10;
+    const sessionNumber = useSelector(getSessionPoints) / 10;
+    const unexpectedEnd = 1;
+
+    const sessionModifications = useSelector(getModificationsList).includes('one') ? 1.5 : 1;
+    const sign = useSelector(getExampleSignList);
+    const sessionSign = sign.includes('*') || sign.includes('/') ? 2.0
+        : sign.some((el) => ['-', '+'].includes(el)) ? 1.5 : 1.75;
+
+    const chance = (baseChance * baseMultiplier ** (sessionTime
+            * sessionModifications + sessionNumber + sessionSign) * unexpectedEnd);
+
+    const resultChance = Math.min(chance, maxChance);
+
+    const calculateBoosterPackChance = () => {
+
+        const randomNumber = Math.random() * 100;
+        return randomNumber <= resultChance;
+        
+    };
+
+    return { calculateBoosterPackChance };
 };
